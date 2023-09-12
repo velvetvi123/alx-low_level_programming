bfd88011a72d3a2f24d957ff9f09086e77bc72a6
velvetvi123 <vic_velvet@yahoo.com>
2023-09-11 18:23:10 -0700

    yahya

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,12 @@
+#include <stdlib.h>
+#include "dog.h"
+
+
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	if (d == NULL)
+		d = malloc(sizeof(struct dog));
+	d->name = name;
+	d->age = age;
+	d->owner = owner;
+}
