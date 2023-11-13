19a62f5fc762d5d6dc546ec721f7b9b7ff88145e
velvetvi123 <vic_velvet@yahoo.com>
2023-11-13 15:57:31 -0800

    job

---
+++ 0x18-dynamic_libraries/main.h
@@ -0,0 +1,17 @@
+#ifndef MAIN_H 
+#define MAIN_H 
+#include <stdio.h>
+#include <stdlib.h>
+int _putchar(char c);
+int _islower(int c);
+int _isalpha(int c); 
+int _abs(int n);
+void _puts(char *str);
+int _strlen(char *s); 
+char *_strcpy(char *dest, char *src); 
+int _atoi(char *s); 
+char *_strcat(char *dest, char *src); 
+unsigned int _strspn(char *s, char *accept); 
+char *_strpbrk(char *s, char *accept); 
+char *_strstr(char *haystack, char *needle);
+#endif /* MAIN_H */
